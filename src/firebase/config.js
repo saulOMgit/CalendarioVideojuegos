51c9f322a4dc191ac201a61a13245c2d2ab98805
saulOMgit <sauloteromelchor@gmail.com>
2024-06-06 19:27:28 +0200

    dotenv aplicado

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,19 +3,15 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAHWiMv_KlclLrIjdPWN4xsRSnOfGrot18",
-  authDomain: "videogame-calendar.firebaseapp.com",
-  projectId: "videogame-calendar",
-  storageBucket: "videogame-calendar.appspot.com",
-  messagingSenderId: "137655325919",
-  appId: "1:137655325919:web:71c4ce71aa1493639b856a",
-  measurementId: "G-3ZWR8X195F"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
